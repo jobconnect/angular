82cb5f5215fe5106adea512a7b562c9ec7e9ecb8
shine911 <deathbook1999@gmail.com>
2019-03-28 08:16:40 +0700

    all working

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -9,13 +9,25 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { EmployeeService } from '../jobseeker/resolve/employee.service';
+import { InterviewEtiquetteService } from '../jobseeker/resolve/interview-etiquette.service';
+import { InterviewEtiquetteComponent } from '../jobseeker/interview-etiquette/interview-etiquette.component';
+import { VoidInterviewComponent } from '../jobseeker/void-interview/void-interview.component';
+import { DetailsComponent } from '../jobseeker/void-interview/details/details.component';
+import { AboutComponent } from '../jobseeker/about/about.component';
+import { ContactComponent } from '../jobseeker/contact/contact.component';
+import { InterviewVoidService } from '../jobseeker/resolve/interview-void.service';
 
 //Định nghĩa router riêng cho module này
 const routing: Routes = [{ 
   path: '', component: EmployerComponent, children: [
     {path: '', redirectTo: 'home'},
     {path: 'home', component: HomeComponent, resolve: {'emp':EmployeeService}},
-    {path: 'find', component: FindComponent, resolve: {'emp': EmployeeService}}
+    {path: 'find', component: FindComponent, resolve: {'emp': EmployeeService}},
+    {path: 'interview-eiquette', component: InterviewEtiquetteComponent, resolve: {'video': InterviewEtiquetteService}},
+    {path: 'void-interview', component: VoidInterviewComponent, resolve: {'post': InterviewVoidService}},
+    {path: 'void-interview/:id', component: DetailsComponent, resolve: {'post': InterviewVoidService}},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent}
   ]
 }];
 
@@ -23,7 +35,7 @@ const routing: Routes = [{
 //cho nên chúng ta định nghĩa forChild cho router này
 const Routing: ModuleWithProviders = RouterModule.forChild(routing);
 @NgModule({
-  declarations: [EmployerComponent, HomeComponent, FindComponent, NavbarComponent, FooterComponent],
+  declarations: [EmployerComponent, HomeComponent, FindComponent, NavbarComponent, FooterComponent, InterviewEtiquetteComponent, VoidInterviewComponent, DetailsComponent, AboutComponent, ContactComponent],
   imports: [
     CommonModule,
     Routing,
